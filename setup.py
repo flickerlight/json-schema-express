bb6fe6b811437c8c832564c23cdc040804fef669
Lynn Chen <cwlme82@gmail.com>
2016-01-08 11:01:24 +0800

    add setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='json_schema_express',
+      version='0.1',
+      description='Python library for generating fake data according to json schema',
+      author='Lynn Chen',
+      author_email='cwlme82@gmail.com',
+      packages=['json_schema_express']
+      )
